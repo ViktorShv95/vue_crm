cc04c7996f1b3b52a6dd2ec92ba4500c0651826c
Viktor Shvaikovskii <viktor.shv1995@gmail.com>
2019-06-06 17:44:42 +0700

    Added planing section

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,11 +2,11 @@
   <div>
     <Loader v-if="loading" />
     <div class="app-main-layout" v-else>
-
+      
       <Navbar @click="isOpen = !isOpen" />
 
       <Sidebar v-model="isOpen" />
-
+      
       <main class="app-content" :class="{full: !isOpen}">
         <div class="app-page">
           <router-view />
@@ -14,7 +14,7 @@
       </main>
 
       <div class="fixed-action-btn">
-        <router-link class="btn-floating btn-large blue" to="/record">
+        <router-link class="btn-floating btn-large blue" to="/record" v-tooltip="'Создать новую запись'">
           <i class="large material-icons">add</i>
         </router-link>
       </div>
